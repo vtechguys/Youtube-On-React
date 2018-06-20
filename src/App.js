c6f4cd7e293e4d6610d4ac01993a7aaa2b368d13
Aniket Jha <vtechguys@gmail.com>
2018-06-20 16:01:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component{
 
     render(){
         return (
-            <BrowserRouter basename="https://vtechguys.github.io/Youtube-On-React/">
+            <BrowserRouter basename={"https://vtechguys.github.io/Youtube-On-React/"}>
                 <div id={this.state.appName}>
                     <Layout {...this.state}/>
                 </div>
