08af95686ec3b7204dd9c9525d90ec220ef827d8
Aniket Jha <vtechguys@gmail.com>
2018-06-20 15:56:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component{
 
     render(){
         return (
-            <BrowserRouter>
+            <BrowserRouter basename="http://vtechguys.github.io/">
                 <div id={this.state.appName}>
                     <Layout {...this.state}/>
                 </div>
